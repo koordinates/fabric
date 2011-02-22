a810836235382d4db0700fc64cf3e913a4dedcba
Travis Cline <travis.cline@gmail.com>
2011-02-22 14:26:52 -0600

    removed unused imports from test_network

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,19 +1,13 @@
 from __future__ import with_statement
 
-from datetime import datetime
-import copy
-import getpass
 import sys
 
 import paramiko
-from nose.tools import with_setup
-from fudge import (Fake, clear_calls, clear_expectations, patch_object, verify,
-    with_patched_object, patched_context, with_fakes)
+from fudge import Fake, patch_object, with_fakes
 
 from fabric.context_managers import settings, hide, show
 from fabric.network import (HostConnectionCache, join_host_strings, normalize,
     denormalize)
-from fabric.io import output_loop
 import fabric.network # So I can call patch_object correctly. Sigh.
 from fabric.state import env, output, _get_system_username
 from fabric.operations import run, sudo
